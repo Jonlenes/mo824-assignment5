dc56d2cf605a9c2aa367700d3ce1a9114a63b105
Jonlenes Silva Castro <jonlenes.castro@minerva.local>
2020-11-09 15:28:22 -0300

    Experiment class

---
+++ src/models/Experiment.java
@@ -0,0 +1,19 @@
+package models;
+
+public class Experiment {
+	private String key;
+	
+	public Experiment(String key) {
+		super();
+		this.key = key;
+	}
+	
+	public String getKey() {
+		return key;
+	}
+	
+	public void setKey(String key) {
+		this.key = key;
+	}
+	
+}
